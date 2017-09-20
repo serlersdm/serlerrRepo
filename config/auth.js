2ce04a99200609fd1908fa4afcffe89b348d4e38
serlersdm <serlersdm@gmail.com>
2017-09-20 14:32:49 +1200

    new facebook access keys added

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,8 @@
 module.exports = {
   facebookAuth: {
-    clientID: '896605267054808',
-    clientSecret: '28f6a7d2842d5f9ba19e449131694902',
+    clientID: '272705306568506',
+    clientSecret: 'd38c4be212f7dca76a97484030eedf03',
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
-  },
-  twitterAuth: {
-    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
-    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback',
   },
   googleAuth: {
     clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
