8c9bd611915fb0a31864ff2f20132e1348faad48
serlersdm <serlersdm@gmail.com>
2017-10-05 06:02:15 +1300

    routing changes

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
   facebookAuth: {
     clientID: '272705306568506',
     clientSecret: 'd38c4be212f7dca76a97484030eedf03',
-    callbackURL: 'http://localhost:8080/auth/facebook/callback',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
   },
   googleAuth: {
     clientID: '194986861349-javaa1msvh0sbg3n6dineegfiuvhmq9l.apps.googleusercontent.com',
     clientSecret: '3EpeOE2C1Wcu4p4D33bL5nrV',
-    callbackURL: 'http://localhost:8080/auth/google/callback',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
   },
 };
