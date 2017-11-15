5cba97ba179dd103da89fc75f2ae5e0fcfed749b
serlersdm <serlersdm@gmail.com>
2017-11-15 18:50:25 +0530

    post
    xyz

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,7 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true,
 }));
 
-router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+router.post('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 router.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/loggedin',
